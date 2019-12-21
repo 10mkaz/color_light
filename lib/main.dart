b3bade225a64c34dc80db2cb132528d2e4745940
10mkaz <46083704+10mkaz@users.noreply.github.com>
2019-12-21 16:33:23 +0900

    Add color change feature

--- lib/main.dart
+++ lib/main.dart
@@ -6,56 +6,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHome(title: 'Color Light'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyHome extends StatefulWidget {
+  MyHome({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomeState createState() => _MyHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomeState extends State<MyHome> {
+  List<Color> _colorList = [
+    Colors.black,
+    Colors.green,
+    Colors.blue,
+    Colors.pink,
+    Colors.red,
+    Colors.yellow,
+    Colors.grey,
+    Colors.white,
+  ];
+  int _nowListNo = 0;
+  Color _backColor = Colors.black;
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
+      _nowListNo++;
+      if ( _nowListNo >= _colorList.length){
+        _nowListNo=0;
+      }
+      _backColor = _colorList[_nowListNo];
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+      backgroundColor: _backColor,
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
